77a631b8a0034feb00f6f35a10c75e4af1a68110
hmy-repo <hengmaoyue@gmail.com>
2024-05-19 09:11:23 +0900

    update the chart and image and rgb con tableview

--- detectmain.py
+++ detectmain.py
@@ -4,13 +4,24 @@ import sys
 from PySide6.QtWidgets import (QMainWindow, QFileDialog,QColorDialog, QComboBox,
                                 QDialog, QFontComboBox,QTextEdit, QInputDialog,
                                 QLineEdit, QMenu, QMessageBox,QProgressBar, QToolBar,
-                                QVBoxLayout, QWidget, QTreeView, QFileSystemModel)
-from PySide6.QtGui import QAction, QGuiApplication, QIcon, QKeySequence
-from PySide6.QtCore import QUrl, Qt, Slot, Signal, QDir
+                                QVBoxLayout, QWidget, QTreeView, QTableView, QFileSystemModel,
+                                QHeaderView)
+from PySide6.QtGui import (QAction, QGuiApplication, QIcon, QKeySequence, QStandardItemModel,
+                            QStandardItem, QImage, QPixmap)
+from PySide6.QtCore import (QUrl, Qt, Slot, Signal, QDir)
 
 from PySide6.QtPrintSupport import (QAbstractPrintDialog, QPrinter,
                                     QPrintDialog, QPrintPreviewDialog)
+from PySide6.QtSql import QSqlTableModel
 from PySide6.QtGui import QIcon
+#from tr import tr
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
+from matplotlib.figure import Figure
+from scipy import stats
+
 from ui import ui_detectmain
 from camera import Camera
 
@@ -26,6 +37,122 @@ class DetectMain(QWidget):
 #        self.ui.cameraMainGroupBox.setWidget(self.mainCamera._ui.cameraWidget)
         self.ui.cameraMainlVLayout.addWidget(self.mainCamera._ui.cameraWidget)
         self.ui.cameraMainlVLayout.setContentsMargins(0,0,0,0)
+
+
+        #
+        self.layout = QVBoxLayout()
+        self.ui.widgetChart.setLayout(self.layout)
+
+        # canvas
+#        self.figure = plt.figure()
+        self.figure, self.ax = plt.subplots(figsize=(15,15), dpi=100)#nrows=1, ncols=1,figsize=(15,10))#figsize=(20,15))
+        self.canvas = FigureCanvasQTAgg(self.figure)
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
+        self.layout.addWidget(self.canvas)
+#        self.layout.addWidget(self.toolbar)
+
+        self.origImg ='./imgs/origImg.jpg'
+        self.recgImg ='./imgs/recgImg.jpg'
+
+        self.ui.labelOrigImg.setPixmap(QPixmap(self.origImg).scaled(self.ui.labelOrigImg.width(), self.ui.labelOrigImg.height(), Qt.KeepAspectRatio))
+        self.ui.labelRecgImg.setPixmap(QPixmap(self.recgImg).scaled(self.ui.labelRecgImg.width(), self.ui.labelRecgImg.height(), Qt.KeepAspectRatio))
+
+
+        # tableview for the original img, the concentration and RGB are known, to get the chart
+        x = [1, 10, 30, 50, 80, 100, 120]
+        y = [156, 144, 126, 103, 73, 53, 29]
+        con_rgb_list = []
+        for i in range(len(x)):
+            con_rgb_list.append(str(x[i]))
+            con_rgb_list.append(str(y[i]))
+        model = QStandardItemModel()
+        model.setColumnCount(2)
+        for row in range(len(x)):
+            for col in range(2):
+                index = row*2+col
+                item = QStandardItem(con_rgb_list[index])
+                model.setItem(row, col, item)
+
+        model.setHeaderData(0, Qt.Horizontal, "Con")
+        model.setHeaderData(1, Qt.Horizontal, "Blue")
+
+        self.ui.tabviewOrig.setModel(model)
+        self.ui.tabviewOrig.verticalHeader().hide()
+#        self.ui.tabviewOrig.setHorizontalHeader(QHeaderView())
+        self.ui.tabviewOrig.setColumnWidth(0, self.ui.tabviewOrig.width()/2+16)
+        self.ui.tabviewOrig.setColumnWidth(1, self.ui.tabviewOrig.width()/2+16)
+
+
+
+        # plot the Chart
+        font = {'family': 'serif',
+                'color':  'black',
+                'weight': 'normal',
+                'size': 13,
+                }
+
+        x = [1, 10, 30, 50, 80, 100, 120]
+        y = [156, 144, 126, 103, 73, 53, 29]
+        slope, intercept, r, p, std_err = stats.linregress(x, y)
+        def myfunc(x):
+            return slope*x+intercept
+        mymodel = list(map(myfunc, x))
+
+        text = "y = {:.2f}*x+{:.2f}".format(slope, intercept)
+        print('text=', text)
+#        print("y = {:.2f}*x+{:.2f}".format(slope, intercept))
+
+        rgb_B = [145, 111, 93, 50, 143, 114, 92, 48]
+        con = []
+        for item_B in rgb_B:
+            con_temp = (item_B - intercept)/slope
+            con.append(con_temp)
+
+#        plt.plot(x, y, 'k')
+        plt.title('Linear Regression and ML-based Fast Prediction', fontdict=font)
+
+        plt.text(71, 143, "y = {:.2f} * x+{:.2f}".format(slope, intercept),
+                backgroundcolor='#069AF3', fontsize=13,
+                fontstyle='italic', fontfamily='times new roman',
+                color=(1, 1, 1, 1))
+#        plt.text(61, 143, r'$\cos(2 \pi t) \exp(-t)$', fontdict=font)
+        plt.xlabel('Concentration %', fontdict=font)
+        plt.ylabel('Blue Value', fontdict=font)
+
+
+
+        plt.scatter(x,y, color='#0343DF')
+        plt.plot(x, mymodel, color='#069AF3')
+        plt.scatter(con, rgb_B, color='#ff7f0e')
+        plt.legend(('experiment data', 'linear regression', 'prediction'),
+                   loc='lower left', shadow=True)
+        for x, y in zip(con, rgb_B):
+            plt.text(x, y, '({:.1f},{:.1f})'.format(x,y),fontsize=7,rotation=30,color='#ff7f0e') #f'(x: {x}, y: {y})')
+        plt.show()
+
+
+
+        # tableview for the recognized img, the concentration and RGB are NOT known
+        rgb_con_list = []
+        for i in range(len(rgb_B)):
+            rgb_con_list.append(str(round(rgb_B[i],1)))
+            rgb_con_list.append(str(round(con[i],1)))
+        model = QStandardItemModel()
+        model.setColumnCount(2)
+        for row in range(len(rgb_B)):
+            for col in range(2):
+                index = row*2+col
+                item = QStandardItem(rgb_con_list[index])
+                model.setItem(row, col, item)
+
+        model.setHeaderData(0, Qt.Horizontal, "Blue")
+        model.setHeaderData(1, Qt.Horizontal, "Con")
+
+        self.ui.tabviewRecg.setModel(model)
+        self.ui.tabviewRecg.verticalHeader().hide()
+#        self.ui.tabviewRecg.setHorizontalHeader(QHeaderView())
+        self.ui.tabviewRecg.setColumnWidth(0, self.ui.tabviewRecg.width()/2+16)
+        self.ui.tabviewRecg.setColumnWidth(1, self.ui.tabviewRecg.width()/2+16)
 
 
 
--- ui/ui_detectmain.py
+++ ui/ui_detectmain.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGraphicsView, QGridLayout,
-    QGroupBox, QHBoxLayout, QLCDNumber, QLabel,
-    QListView, QProgressBar, QPushButton, QScrollArea,
-    QSizePolicy, QTabWidget, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
+    QHBoxLayout, QHeaderView, QLCDNumber, QLabel,
+    QProgressBar, QPushButton, QSizePolicy, QTabWidget,
+    QTableView, QVBoxLayout, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -57,16 +57,10 @@ class Ui_Form(object):
         self.tab_4.setObjectName(u"tab_4")
         self.horizontalLayout_5 = QHBoxLayout(self.tab_4)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.scrollArea = QScrollArea(self.tab_4)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setFrameShape(QFrame.Panel)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 692, 317))
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.labelOrigImg = QLabel(self.tab_4)
+        self.labelOrigImg.setObjectName(u"labelOrigImg")
 
-        self.horizontalLayout_5.addWidget(self.scrollArea)
+        self.horizontalLayout_5.addWidget(self.labelOrigImg)
 
         self.tabWidget_2.addTab(self.tab_4, "")
 
@@ -82,10 +76,10 @@ class Ui_Form(object):
 
         self.verticalLayout_8.addWidget(self.label_2)
 
-        self.listView = QListView(self.groupBox_4)
-        self.listView.setObjectName(u"listView")
+        self.tabviewOrig = QTableView(self.groupBox_4)
+        self.tabviewOrig.setObjectName(u"tabviewOrig")
 
-        self.verticalLayout_8.addWidget(self.listView)
+        self.verticalLayout_8.addWidget(self.tabviewOrig)
 
 
         self.horizontalLayout_6.addLayout(self.verticalLayout_8)
@@ -104,52 +98,42 @@ class Ui_Form(object):
         self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
         self.verticalLayout_12 = QVBoxLayout()
         self.verticalLayout_12.setObjectName(u"verticalLayout_12")
-        self.widget = QWidget(self.groupBox_6)
-        self.widget.setObjectName(u"widget")
+        self.widgetChart = QWidget(self.groupBox_6)
+        self.widgetChart.setObjectName(u"widgetChart")
 
-        self.verticalLayout_12.addWidget(self.widget)
+        self.verticalLayout_12.addWidget(self.widgetChart)
 
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.pushButton_3 = QPushButton(self.groupBox_6)
-        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton = QPushButton(self.groupBox_6)
+        self.pushButton.setObjectName(u"pushButton")
 
-        self.gridLayout.addWidget(self.pushButton_3, 0, 0, 1, 1)
-
-        self.pushButton_5 = QPushButton(self.groupBox_6)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-
-        self.gridLayout.addWidget(self.pushButton_5, 0, 1, 1, 1)
-
-        self.pushButton_2 = QPushButton(self.groupBox_6)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-
-        self.gridLayout.addWidget(self.pushButton_2, 0, 2, 1, 1)
-
-        self.pushButton_7 = QPushButton(self.groupBox_6)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-
-        self.gridLayout.addWidget(self.pushButton_7, 0, 3, 1, 1)
+        self.gridLayout.addWidget(self.pushButton, 1, 4, 1, 1)
 
         self.pushButton_4 = QPushButton(self.groupBox_6)
         self.pushButton_4.setObjectName(u"pushButton_4")
 
-        self.gridLayout.addWidget(self.pushButton_4, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_4, 1, 2, 1, 1)
 
-        self.pushButton_6 = QPushButton(self.groupBox_6)
-        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_5 = QPushButton(self.groupBox_6)
+        self.pushButton_5.setObjectName(u"pushButton_5")
 
-        self.gridLayout.addWidget(self.pushButton_6, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_5, 1, 1, 1, 1)
 
-        self.pushButton = QPushButton(self.groupBox_6)
-        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton_7 = QPushButton(self.groupBox_6)
+        self.pushButton_7.setObjectName(u"pushButton_7")
 
-        self.gridLayout.addWidget(self.pushButton, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_7, 1, 3, 1, 1)
 
         self.pushButton_8 = QPushButton(self.groupBox_6)
         self.pushButton_8.setObjectName(u"pushButton_8")
 
-        self.gridLayout.addWidget(self.pushButton_8, 1, 3, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_8, 1, 5, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.groupBox_6)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout.addWidget(self.pushButton_3, 1, 0, 1, 1)
 
 
         self.verticalLayout_12.addLayout(self.gridLayout)
@@ -177,12 +161,10 @@ class Ui_Form(object):
         self.tab_3.setObjectName(u"tab_3")
         self.horizontalLayout_3 = QHBoxLayout(self.tab_3)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.graphicsView = QGraphicsView(self.tab_3)
-        self.graphicsView.setObjectName(u"graphicsView")
-        self.graphicsView.setFrameShape(QFrame.Panel)
-        self.graphicsView.setLineWidth(1)
+        self.labelRecgImg = QLabel(self.tab_3)
+        self.labelRecgImg.setObjectName(u"labelRecgImg")
 
-        self.horizontalLayout_3.addWidget(self.graphicsView)
+        self.horizontalLayout_3.addWidget(self.labelRecgImg)
 
         self.tabWidget.addTab(self.tab_3, "")
 
@@ -239,12 +221,10 @@ class Ui_Form(object):
 
         self.verticalLayout_9.addWidget(self.label_4)
 
-        self.Res01_Num08_Ser1jpg = QListView(self.groupBox_3)
-        self.Res01_Num08_Ser1jpg.setObjectName(u"Res01_Num08_Ser1jpg")
-        self.Res01_Num08_Ser1jpg.setFrameShape(QFrame.Panel)
-        self.Res01_Num08_Ser1jpg.setSelectionRectVisible(False)
+        self.tabviewRecg = QTableView(self.groupBox_3)
+        self.tabviewRecg.setObjectName(u"tabviewRecg")
 
-        self.verticalLayout_9.addWidget(self.Res01_Num08_Ser1jpg)
+        self.verticalLayout_9.addWidget(self.tabviewRecg)
 
 
         self.horizontalLayout_4.addLayout(self.verticalLayout_9)
@@ -282,18 +262,18 @@ class Ui_Form(object):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.cameraMainGroupBox.setTitle("")
         self.groupBox_4.setTitle("")
+        self.labelOrigImg.setText("")
         self.tabWidget_2.setTabText(self.tabWidget_2.indexOf(self.tab_4), QCoreApplication.translate("Form", u"Original Image", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"Import from file:", None))
         self.groupBox_6.setTitle("")
-        self.pushButton_3.setText(QCoreApplication.translate("Form", u"Import Data", None))
-        self.pushButton_5.setText(QCoreApplication.translate("Form", u"Load Original Img", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Form", u"Single Detect", None))
-        self.pushButton_7.setText(QCoreApplication.translate("Form", u"Export PDF", None))
-        self.pushButton_4.setText(QCoreApplication.translate("Form", u"Plot Figure", None))
-        self.pushButton_6.setText(QCoreApplication.translate("Form", u"Load Original Imgs", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"Batch Detect", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Form", u"Plot Figure", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Form", u"Load Img", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Form", u"Export PDF", None))
         self.pushButton_8.setText(QCoreApplication.translate("Form", u"Save Results", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"Import Data", None))
         self.groupBox_3.setTitle("")
+        self.labelRecgImg.setText("")
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("Form", u"Recgnition Results:", None))
         self.label_5.setText(QCoreApplication.translate("Form", u"Progress:", None))
         self.label_9.setText(QCoreApplication.translate("Form", u"Period:", None))
